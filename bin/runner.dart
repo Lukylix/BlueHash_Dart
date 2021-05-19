5ffbf836238bf009c5bbfb4a04ff981df3de12ed
Lukylix <lucas.garcia.bobi@gmail.com>
2021-05-19 17:59:39 +0200

    Miner restart on crash

--- bin/runner.dart
+++ bin/runner.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import '../config/miners.dart';
 import 'package:process_run/shell.dart';
 
@@ -7,25 +6,33 @@ final bin =
     "../miners_binary/PhoenixMiner/PhoenixMiner_5.6d_Linux/PhoenixMiner ";
 final args =
     "-pool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -wal ${walletBTC}.1070 -pass x -proto 4 -stales 0";
+
+var running = true;
+int pid;
 main() async {
+  runner(bin, args);
+}
+
+runner(String bin, String args) async {
   var shell = Shell();
+  await shell
+      .cd('log')
+      .run('gnome-terminal -- sh -c "chmod +x ${bin}; ${bin + args}"');
+  watcher("PhoenixMiner");
+}
 
-  await shell.cd('log').run('gnome-terminal -- sh -c "${bin + args}"');
-
-  var controller = ShellLinesController();
-  var shell2 = Shell(stdout: controller.sink, verbose: false);
-  var pid;
-  controller.stream.listen((event) {
-    print("event: ${event.split(" ").first}");
-    pid = int.parse(event);
+watcher(String miner) {
+  Future.delayed(Duration(seconds: 5), () async {
+    var controller = ShellLinesController();
+    var shell = Shell(stdout: controller.sink, verbose: false);
+    await shell.run("pidof ${miner}").onError((error, stackTrace) {
+      runner(bin, args);
+      return;
+    });
+    controller.stream.listen((event) {
+      pid = int.parse(event.split(" ")[0]);
+      if (running) watcher(miner);
+      if (!running) Process.killPid(pid);
+    });
   });
-
-  Future.delayed(Duration(seconds: 3), () async {
-    await shell2.run("pidof PhoenixMiner");
-    print("PID: ${pid}");
-    shell2.kill();
-    // Process.killPid(pid);
-  });
-
-  
 }
