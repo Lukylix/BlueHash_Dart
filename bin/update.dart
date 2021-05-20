a7ce3f278425fcee661d1291a54e995abbadf3b2
Lukylix <lucas.garcia.bobi@gmail.com>
2021-05-20 21:04:02 +0200

    Donwload link graber update

--- bin/update.dart
+++ bin/update.dart
@@ -7,10 +7,22 @@ const Map minersLinks = const {
   'NanoMiner':
       "https://api.github.com/repos/nanopool/nanominer/releases/latest",
   'NBMiner': "https://api.github.com/repos/NebuTech/NBMiner/releases/latest",
+  'GMiner':
+      "https://api.github.com/repos/develsoftware/GMinerRelease/releases/latest",
+  'CryptoDredge':
+      "https://api.github.com/repos/technobyl/CryptoDredge/releases",
 };
 main(List<String> args) {
-  downlinkfinderGithub("NBMiner");
-  downlinkfinderGithub("NanoMiner");
+  // downlinkfinder("NBMiner");
+  // downlinkfinder("NanoMiner");
+  // downlinkfinder("GMiner");
+  // downlinkfinder('CryptoDredge');
+  // downlinkfinder("PhoenixMiner");
+}
+
+downlinkfinder(String miner) {
+  if (minersLinks[miner].contains("github")) return downlinkfinderGithub(miner);
+  return downlinkfinderHtml(miner);
 }
 
 downlinkfinderGithub(String miner) async {
@@ -18,7 +30,9 @@ downlinkfinderGithub(String miner) async {
   var response = await http.get(url);
   var decoded = jsonDecode(response.body);
   var links = [];
-  for (var asset in decoded["assets"]) {
+  for (var asset in url.toString().contains("latest")
+      ? decoded["assets"]
+      : decoded[0]["assets"]) {
     if (Platform.isLinux &&
         asset["browser_download_url"]
             .contains(RegExp(r'linux', caseSensitive: false))) {
@@ -27,6 +41,28 @@ downlinkfinderGithub(String miner) async {
         links.add(asset["browser_download_url"]);
     }
   }
-  print(links);
+  return links;
+}
+
+downlinkfinderHtml(String miner) async {
+  var url = Uri.parse(minersLinks[miner]);
+  var response = await http.get(url);
+  var reg = RegExp(r'href="(.*)"', caseSensitive: false, multiLine: true);
+  var links = [];
+  var matches = reg.allMatches(response.body);
+  for (var item in matches) {
+    if (item.group(1).contains(miner) &&
+        item.group(1).toLowerCase().contains("linux") &&
+        !item
+            .group(1)
+            .toLowerCase()
+            .contains(RegExp(r'\.asc$', caseSensitive: false)))
+      links.add(item.group(1));
+  }
+  //Relative link into absolute
+  for (var i = 0; i < links.length; i++) {
+    if (!links[i].contains(RegExp(r'^http')))
+      links[i] = minersLinks[miner] + links[i];
+  }
   return links;
 }
